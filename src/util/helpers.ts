08ec5a33c98868375973bbe74e958469ca89b36e
carissaor <carissaor@gmail.com>
2024-09-27 09:27:43 -0700

    handle more than 1 file in courses folder better

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -14,7 +14,7 @@ export function idValidator(id: string): boolean {
 	return true;
 }
 
-export async function readContent(content: string): Promise<any> {
+export async function readContent(content: string): Promise<any[]> {
 	let zipFile;
 
 	try {
@@ -38,24 +38,33 @@ export async function readContent(content: string): Promise<any> {
 
 	// Iterating through all the files under zip file
 	for (const filename of Object.keys(zipFile.files)) {
-		// Process file that follows the path "courses/" and is not a directory
-		if (filename.startsWith("courses/") && !zipFile.files[filename].dir) {
+		// Process file that follows the path "courses/" and is not a directory (+ not .DS_Store) - not sure if there is a better way to do this
+		if (filename.startsWith("courses/") && !zipFile.files[filename].dir && !filename.endsWith(".DS_Store")) {
 			// Turn the file into text then check the validity of the section
 			const section = zipFile.files[filename].async("text").then((fileContent) => {
-				const courseJson = JSON.parse(fileContent);
-				// Check if the jsonData is valid
-				if (courseValidator(courseJson)) {
-					return courseJson.result;
-				} else {
-					throw new InsightError("No valid course found.");
-				}
+				return processFile(fileContent);
 			});
 			allPromises.push(section);
 		}
 	}
-
-	// for now results contains the all json objects
+	// wait for all file to process
 	return await Promise.all(allPromises);
+}
+
+function processFile(fileContent: any): any {
+	try {
+		// problem here when there are more than 1 file in courses
+		// console.log(fileContent);
+		const courseJson = JSON.parse(fileContent);
+		// Check if the jsonData is valid
+		if (courseValidator(courseJson)) {
+			return courseJson.result;
+		} else {
+			throw new InsightError("No valid course found.");
+		}
+	} catch (err) {
+		throw new InsightError(err instanceof Error ? err.message : String(err));
+	}
 }
 
 function courseValidator(course: any): boolean {
