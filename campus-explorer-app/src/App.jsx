ce129d5114db6de9cf17cacc14ba9b74028cd259
joshua <josh_jay23@hotmail.com>
2024-11-12 23:53:07 -0800

    added map focused on UBC on the homepage

--- campus-explorer-app/src/App.jsx
+++ campus-explorer-app/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Navbar/>
-    </>
-  )
+  const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+	return (
+		<>
+			<Navbar/>
+			<div className="d-flex justify-content-center">
+      <APIProvider apiKey={mapsApiKey}>
+				<Map
+					style={{ width: "50vw", height: "50vh" }}
+					defaultCenter={{ lat: 49.26115791687482, lng: -123.24602244318108 }} // UBC: 49.26115791687482, -123.24602244318108
+					defaultZoom={14}
+					gestureHandling={"greedy"}
+					disableDefaultUI={true}
+				/>
+			</APIProvider>
+      </div>
+		</>
+	);
 }
 
-export default App
+export default App;
