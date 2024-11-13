9722f5e574e6ea717e718d11752b6c74d3833fa4
joshua <josh_jay23@hotmail.com>
2024-11-13 00:41:19 -0800

    added sample building points on map

--- campus-explorer-app/src/App.jsx
+++ campus-explorer-app/src/App.jsx
@@ -1,27 +1,15 @@
-import { useState } from "react";
 import "./App.css";
+import React from "react";
+// import { APIProvider, Map, AdvancedMarker, MapCameraChangedEvent, Pin } from "@vis.gl/react-google-maps";
 
 import Navbar from "./components/Navbar";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import MapWithMarkers from "./components/MapWithMarkers";
 
 function App() {
-  const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 	return (
 		<>
 			<Navbar/>
-			<div className="d-flex justify-content-center">
-      <APIProvider apiKey={mapsApiKey}>
-				<Map
-					style={{ width: "50vw", height: "50vh" }}
-					defaultCenter={{ lat: 49.26115791687482, lng: -123.24602244318108 }} // UBC: 49.26115791687482, -123.24602244318108
-					defaultZoom={14}
-					gestureHandling={"greedy"}
-					disableDefaultUI={true}
-				/>
-			</APIProvider>
-      </div>
+      <MapWithMarkers/>
 		</>
 	);
 }
