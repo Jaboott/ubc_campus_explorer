b38fbdb3b1f0edff29dd0ab92210fd3fdc37769a
joshua <josh_jay23@hotmail.com>
2024-11-13 17:37:01 -0800

    Integrate Vite frontend with Express server

--- frontend/campus-explorer-app/src/App.jsx
+++ frontend/campus-explorer-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 	return (
 		<>
 			<Navbar />
-			{/* <MapWithMarkers /> */}
+			<MapWithMarkers />
 			{/* <Router>
 				<Routes>
 					<Route exact path="/" element={<Home />} />
