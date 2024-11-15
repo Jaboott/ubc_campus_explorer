45b27018bb0bd9e8e3e844916f3d66cbe3716b34
carissaor <carissaor@gmail.com>
2024-11-14 18:28:55 -0800

    Include markers on the map for every building in the dataset

--- frontend/campus-explorer-app/src/components/MapWithMarkers.jsx
+++ frontend/campus-explorer-app/src/components/MapWithMarkers.jsx
@@ -1,46 +1,88 @@
+import React, { useEffect, useState } from "react";
 import { APIProvider, Map, AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
-
-// https://developers.google.com/codelabs/maps-platform/maps-platform-101-react-js#0
-const locations = [
-	{ key: "biology", location: { lat: 49.26481032188259, lng: -123.25129034700498 }}, // 49.26481032188259, -123.25129034700498
-    { key: "buchanan", location: { lat: 49.268717947910126, lng: -123.25462962768056}}, // 49.268717947910126, -123.25462962768056
-    { key: "forestry", location: { lat: 49.2606284047533, lng: -123.24759924515484}} // 49.2606284047533, -123.24759924515484
-];
 
 // https://visgl.github.io/react-google-maps/docs/get-started
 function MapWithMarkers() {
-	const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-	const mapID = "eb2d36cf0095db5e";
-	return (
-		<>
-			<div className="d-flex justify-content-center">
-				<APIProvider apiKey={mapsApiKey}>
-                    <Map
-						style={{ width: "50vw", height: "50vh" }}
-						defaultCenter={{ lat: 49.26115791687482, lng: -123.24602244318108 }} // UBC: 49.26115791687482, -123.24602244318108
-						defaultZoom={14}
-						gestureHandling={"greedy"}
-						// disableDefaultUI={true}
-						mapId={mapID}
-					>
-						<PoiMarkers pois={locations} />
-					</Map>
-				</APIProvider>
-			</div>
-		</>
-	);
+    const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    const mapID = "eb2d36cf0095db5e";
+    const [locations, setLocations] = useState([]);
+
+    // NOTE: Add the dataset using a PUT request before proceeding
+	// Fetch all building information once when the page renders
+	useEffect(() => {
+        const allBuildingsQuery = {
+			"query": {
+				"WHERE": {},
+				"OPTIONS": {
+					"COLUMNS": ["rooms_shortname", "rooms_fullname", "rooms_lat", "rooms_lon"],
+					"ORDER": "rooms_shortname"
+				},
+				"TRANSFORMATIONS": {
+					"GROUP": ["rooms_shortname", "rooms_fullname", "rooms_lat", "rooms_lon"],
+					"APPLY": []
+				}
+			}
+		};		
+		
+		// I'm using my IP address here because for some reason localhost wouldn't work for me...
+		// Otherwise we could just use /query in fetch
+		const apiUrl = "http://128.189.231.98:4321/query";
+
+		fetch(apiUrl, {
+        // fetch("/query", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(allBuildingsQuery)
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                if (data.result) {
+                    const buildingLocations = data.result.map(item => ({
+                        shortname: item.rooms_shortname,
+						fullname: item.rooms_fullname,
+                        location: { lat: item.rooms_lat, lng: item.rooms_lon }
+                    }));
+                    setLocations(buildingLocations);
+                }
+            })
+            .catch(err => {
+                console.log("error:", err.message);
+            });
+    }, []);
+
+    return (
+        <div className="d-flex justify-content-center">
+            <APIProvider apiKey={mapsApiKey}>
+                <Map
+                    style={{ width: "50vw", height: "50vh" }}
+                    defaultCenter={{ lat: 49.26115791687482, lng: -123.24602244318108 }} // UBC: 49.26115791687482, -123.24602244318108
+                    defaultZoom={14}
+                    gestureHandling={"greedy"}
+					// disableDefaultUI={true}
+                    mapId={mapID}
+                >
+                    <PoiMarkers pois={locations} />
+                </Map>
+            </APIProvider>
+        </div>
+    );
 }
 
 // https://developers.google.com/codelabs/maps-platform/maps-platform-101-react-js#0
-const PoiMarkers = (props) => {
-	return (
-		<>
-			{props.pois.map((poi) => (
-				<AdvancedMarker key={poi.key} position={poi.location}>
-					<Pin background={"#FBBC04"} glyphColor={"#000"} borderColor={"#000"} />
-				</AdvancedMarker>
-			))}
-		</>
-	);
-};
+const PoiMarkers = ({ pois }) => (
+    <>
+        {pois.map(poi => (
+            <AdvancedMarker key={poi.key} position={poi.location}>
+                <Pin background={"#FBBC04"} glyphColor={"#000"} borderColor={"#000"} />
+            </AdvancedMarker>
+        ))}
+    </>
+);
+
 export default MapWithMarkers;
