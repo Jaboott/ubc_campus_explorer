311b806475633bc2cf5e0094a1196a149600b608
carissaor <carissaor@gmail.com>
2024-11-19 15:45:35 -0800

    improve error handling for search func

--- frontend/campus-explorer-app/src/pages/Home.jsx
+++ frontend/campus-explorer-app/src/pages/Home.jsx
@@ -5,27 +5,30 @@ import { getAllBuildingsQuery } from "../query/getAllBuildingsQuery";
 import getBuildings from "../hooks/getBuildings";
 
 function Home() {
-  const [buildings, setbuildings] = useState([]);
+	const [buildings, setbuildings] = useState([]);
 
-  // fetch all buildings once when the page renders
-  useEffect(() => {
-    const fetchBuildings = async () => {
-      try {
-        const buildings = await getBuildings(getAllBuildingsQuery());
-        setbuildings(buildings);
-      } catch (err) {
-        console.error("Error fetching buildings:", err);
-      }
-    };
-    fetchBuildings();
-  }, []);
+	// fetch all buildings once when the page renders
+	useEffect(() => {
+		const fetchBuildings = async () => {
+			try {
+				const buildings = await getBuildings(getAllBuildingsQuery());
+				setbuildings(buildings);
+			} catch (err) {
+				console.error("Error fetching buildings:", err);
+			}
+		};
+		fetchBuildings();
+	}, []);
 
-  return (
-    <div className="d-flex flex-column gap-2">
-      <SearchBar setResults={setbuildings} />
-      <MapWithMarkers locations={buildings} />
-    </div>
-  );
+	return (
+		<div className="d-flex flex-column gap-2">
+			<SearchBar setResults={setbuildings} />
+			{buildings.length === 0 && (
+				<div style={{ textAlign: "center", color: "red" }}>No buildings found. Please try again.</div>
+			)}
+			<MapWithMarkers locations={buildings} />
+		</div>
+	);
 }
 
 export default Home;
